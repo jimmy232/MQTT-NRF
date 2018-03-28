b548ea096f875b7ab8567951647be4aec999fbf5
jimmy232 <jamescurriehv@gmail.com>
2018-03-28 22:40:24 +1000

    Update MQTTPub.h

--- MQTTPub.h
+++ MQTTPub.h
@@ -1,3 +1,14 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void mosq_log_callback(struct mosquitto *mosq, void *userdata, int level, const char *str);
 int mqtt_send(char *msg);
 void mqtt_setup();
+
+
+#ifdef __cplusplus
+} // closing brace for extern "C"
+
+#endif
+
