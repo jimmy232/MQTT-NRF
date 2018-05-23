f3a436686c970d1c5a6e2c9eb09f89a09d90a6cf
jimmy232 <jamescurriehv@gmail.com>
2018-05-23 20:26:50 +1000

    Update MQTTSub.c

--- MQTTSub.c
+++ MQTTSub.c
@@ -1,41 +1,49 @@
 #include <signal.h>
 #include <stdio.h>
 #include <stdint.h>
-#include <string.h>
+#include <stdlib.h>
+
+#include <RF24/RF24.h>
+
+#include <sys/types.h>
+#include <unistd.h>
 
 #include <mosquitto.h>
 #include "MQTTSub.h"
+#include <string.h>
+
 
 #define mqtt_host "localhost"
 #define mqtt_port 1883
 
 static int run = 1;
 
+struct package
+{
+//        unsigned long command1 = 68;
+        unsigned long command = 69;
+        unsigned long  macAddr = 14;
+        unsigned long volts = 11;
+        unsigned long amps = 22;
+        unsigned long watts = 33;
+        unsigned long kwh = 44;
+        unsigned long kwh5SecTransmit = 55;
+}RFdata;
+
 void handle_signal(int s)
 {
-	run = 0;
+        run = 0;
 }
 
 void connect_callback(struct mosquitto *mosqSub, void *obj, int result)
 {
-	printf("connect callback, rc=%d\n", result);
+        printf("connect callback, rc=%d\n", result);
 }
 
-void message_callback(struct mosquitto *mosqSub, void *obj, const struct mosquitto_message *message)
+struct mosquitto *mqtt_subscribe()
 {
-	bool match = 0;
-	printf("got message '%.*s' for topic '%s'\n", message->payloadlen, (char*) message->payload, message->topic);
-
-	mosquitto_topic_matches_sub("MyHomeJC/Slave/CMD/", message->topic, &match);
-	if (match) {
-		printf("got message for ADC topic\n");
-	}
-
-}
-									    
-int mqtt_subscribe()
-{
-	uint8_t reconnect = true;
+        printf("Running Subscribe 1..\n");
+        uint8_t reconnect = true;
         char clientid[24];
         struct mosquitto *mosqSub;
         int rc = 0;
@@ -47,29 +55,32 @@ int mqtt_subscribe()
 
         memset(clientid, 0, 24);
         snprintf(clientid, 23, "mysql_log_%d", getpid());
-        mosq = mosquitto_new(clientid, true, 0);
+        mosqSub = mosquitto_new(clientid, true, 0);
 
         if(mosqSub){
+                printf("Running Subscribe 2..\n");
                 mosquitto_connect_callback_set(mosqSub, connect_callback);
                 mosquitto_message_callback_set(mosqSub, message_callback);
 
             rc = mosquitto_connect(mosqSub, mqtt_host, mqtt_port, 60);
 
-                mosquitto_subscribe(mosqSub, NULL, "MyHomeJC/Slave/CMD/", 0);
+                mosquitto_subscribe(mosqSub, NULL, "MyHomeJC/Slave/CMD/#", 0);
 
-                while(run){
-                        rc = mosquitto_loop(mosqSub, -1, 1);
-                        if(run && rc){
-                                printf("connection error!\n");
-                                sleep(10);
-                                mosquitto_reconnect(mosqSub);
-                        }
-                }
-                mosquitto_destroy(mosqSub);
+//                while(run){
+//                      printf("Running Subscribe 3..\n");
+//                        rc = mosquitto_loop(mosqSub, -1, 1);
+//                        if(run && rc){
+//                                printf("connection error!\n");
+//                                sleep(1);
+//                                mosquitto_reconnect(mosqSub);
+//                        }
+//                }
+//                mosquitto_destroy(mosqSub);
         }
 
-        mosquitto_lib_cleanup();
+//        mosquitto_lib_cleanup();
 
-        return rc;	
-	
+//        return rc;
+          return mosqSub;
+
 }
